3002abdf2b03efcb91878808a1e5b26f562081a5
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2025-07-05 11:33:32 +0000

    Adds monitorig to optimization

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -2,6 +2,27 @@
 --SQL CREATE INDEX commands to create appropriate indexes
 --=======================================================
 
+-- analyze before running this script to ensure the database is optimized for performance
+
+EXPLAIN ANALYZE
+SELECT
+    p.property_id,
+    p.name,
+    p.location,
+    COUNT(b.booking_id) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS row_num,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS rank
+FROM
+    properties AS p
+LEFT JOIN
+    bookings AS b ON p.property_id = b.property_id
+GROUP BY
+    p.property_id, p.name, p.location
+ORDER BY
+    COUNT(b.booking_id) DESC,
+    total_bookings DESC,
+    p.name ASC;
+
 -- Indexes for 'users' table
 CREATE INDEX idx_users_email ON users(email);
 
@@ -21,3 +42,24 @@ CREATE INDEX idx_reviews_rating ON reviews (rating);
 CREATE INDEX idx_reviews_created_at ON reviews (created_at DESC);
 CREATE INDEX idx_reviews_prop_id_created_at ON reviews (property_id, created_at DESC);
 
+
+-- analyze after running this script to ensure the database is optimized for performance
+
+EXPLAIN ANALYZE
+SELECT
+    p.property_id,
+    p.name,
+    p.location,
+    COUNT(b.booking_id) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS row_num,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS rank
+FROM
+    properties AS p
+LEFT JOIN
+    bookings AS b ON p.property_id = b.property_id
+GROUP BY
+    p.property_id, p.name, p.location
+ORDER BY
+    COUNT(b.booking_id) DESC,
+    total_bookings DESC,
+    p.name ASC;
